7842b6dfcd4ae218063be40f82c6265d7c08770f
Nicolas Roy B <nrb@atelierfolklore.ca>
2020-03-24 12:17:30 -0400

    Exemples

---
+++ packages/core/src/components/partials/Element.jsx
@@ -0,0 +1,56 @@
+/* eslint-disable react/jsx-props-no-spreading */
+import React from 'react';
+import PropTypes from 'prop-types';
+import { pascalCase } from 'change-case';
+
+import Placeholders from './Placeholders';
+
+// import { PropTypes as MicromagPropTypes } from '@micromag/core';
+
+const propTypes = {
+    name: PropTypes.string.isRequired,
+    components: PropTypes.object.isRequired, // eslint-disable-line
+    props: PropTypes.object, // eslint-disable-line
+    isPlaceholder: PropTypes.bool,
+    className: PropTypes.string,
+    placeholderClassName: PropTypes.string,
+};
+
+const defaultProps = {
+    props: {},
+    isPlaceholder: false,
+    className: null,
+    placeholderClassName: null,
+};
+
+const ElementComponent = ({
+    name,
+    components,
+    props,
+    isPlaceholder,
+    className,
+    placeholderClassName,
+}) => {
+    // console.log(name, props, isPlaceholder, className, placeholderClassName);
+    if (!name) {
+        return 'Bad component name';
+    }
+
+    if (isPlaceholder) {
+        const PlaceholderComponent = Placeholders[pascalCase(name)];
+        return <PlaceholderComponent className={placeholderClassName} />;
+    }
+
+    const RealComponent = components[pascalCase(name)];
+
+    if (!RealComponent) {
+        return 'Bad component';
+    }
+
+    return <RealComponent {...props} className={className} />;
+};
+
+ElementComponent.propTypes = propTypes;
+ElementComponent.defaultProps = defaultProps;
+
+export default ElementComponent;
