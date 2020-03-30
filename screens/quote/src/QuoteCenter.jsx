bd2ca2879afb5956dbf09d69a5d712b465904fd0
Nicolas Roy B <nrb@atelierfolklore.ca>
2020-03-30 12:25:42 -0400

    Refactor Quote

---
+++ screens/quote/src/QuoteCenter.jsx
@@ -0,0 +1,28 @@
+/* eslint-disable react/jsx-props-no-spreading */
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import { PropTypes as MicromagPropTypes } from '@micromag/core';
+import QuoteComponent from './QuoteComponent';
+
+const propTypes = {
+    box: PropTypes.shape({
+        direction: MicromagPropTypes.flexDirection,
+    }),
+};
+
+const defaultProps = {
+    box: {
+        direction: 'column',
+        axisAlign: 'center',
+    },
+};
+
+const QuoteCenter = ({ box, ...otherProps }) => {
+    return <QuoteComponent box={box} {...otherProps} />;
+};
+
+QuoteCenter.propTypes = propTypes;
+QuoteCenter.defaultProps = defaultProps;
+
+export default QuoteCenter;
---
+++ screens/quote/src/components.js
@@ -0,0 +1,5 @@
+export { default as QuoteTop } from './QuoteTop';
+export { default as QuoteCenter } from './QuoteCenter';
+export { default as QuoteBottom } from './QuoteBottom';
+export { default as QuoteTopCentered } from './QuoteTopCentered';
+export { default as QuoteBottomCentered } from './QuoteBottomCentered';
