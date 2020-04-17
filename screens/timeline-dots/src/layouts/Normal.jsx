49b86eae1e17672bd49f9e44f652987836c45207
David Mongeau-Petitpas <dmongeau.petitpas@gmail.com>
2020-04-17 11:58:48 -0400

    Fix with timeline-dots

--- screens/timeline-dots/src/layouts/Normal.jsx
+++ screens/timeline-dots/src/layouts/Normal.jsx
@@ -1,33 +1,20 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
-import PropTypes from 'prop-types';
-import { PropTypes as MicromagPropTypes } from '@micromag/core';
+// import PropTypes from 'prop-types';
+// import { PropTypes as MicromagPropTypes } from '@micromag/core';
 
 import TimelineDotsScreen from '../TimelineDotsScreen';
 
 const propTypes = {
-    title: MicromagPropTypes.text,
-    items: PropTypes.arrayOf(
-        PropTypes.shape({
-            subtitle: MicromagPropTypes.text,
-            text: MicromagPropTypes.text,
-            image: MicromagPropTypes.image,
-        }),
-    ),
-    background: PropTypes.shape({
-        image: MicromagPropTypes.image,
-        color: MicromagPropTypes.color,
-    }),
+
 };
 
 const defaultProps = {
-    title: null,
-    items: null,
-    background: null,
+
 };
 
-const TimelineNormal = ({ items, title, ...otherProps }) => {
-    return <TimelineDotsScreen title={title} items={items} {...otherProps} />;
+const TimelineNormal = (props) => {
+    return <TimelineDotsScreen {...props} />;
 };
 
 TimelineNormal.propTypes = propTypes;
