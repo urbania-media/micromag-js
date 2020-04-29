091754e24aaf3eb9b21cd5fe16598456bf16d10e
David Mongeau-Petitpas <dmongeau.petitpas@gmail.com>
2020-04-29 08:37:46 -0400

    Fix with translations

---
+++ scripts/lib/addMissingTranslations.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const sortIntlMessages = require('./sortIntlMessages');
+
+const addMissingTranslations = (baseLangFile, otherLangFiles) => {
+    const baseTranslations = require(baseLangFile);
+    otherLangFiles.forEach((langPath) => {
+        const translations = require(langPath);
+        const newTranslations = Object.keys(baseTranslations).reduce((map, key) => ({
+            ...map,
+            [key]: translations[key] || `TRANSLATE: ${baseTranslations[key]}`,
+        }), {});
+        const sortedTranslations = sortIntlMessages(newTranslations);
+        fs.writeFileSync(langPath, JSON.stringify(sortedTranslations, null, 4));
+    })
+};
+
+module.exports = addMissingTranslations;
