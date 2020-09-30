1f3655b6efca895772a6966d38ad8bad78e9d4c9
Julien Carignan <carignan.julien@gmail.com>
2020-09-30 15:00:12 -0400

    fix with storybook webpack

--- .storybook/main.js
+++ .storybook/main.js
@@ -16,8 +16,9 @@ module.exports = {
                     test: /\.module\.s[ca]ss$/,
                 },
                 cssLoaderOptions: {
-                    modules: true,
-                    localIdentName: '[path][name]__[local]--[hash:base64:5]',
+                    modules: {
+                        localIdentName: '[name]__[local]--[hash:base64:5]',
+                    },                    
                 },
             },
         },
