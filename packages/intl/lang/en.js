74ade80e5c8b84300af7325a00d1e28300f80b8f
David Mongeau-Petitpas <dmongeau.petitpas@gmail.com>
2020-10-15 17:38:21 -0400

    Added translations

--- packages/intl/lang/en.js
+++ packages/intl/lang/en.js
@@ -4,97 +4,130 @@ var DWEzCv = "Select a video...";
 var EcZYpr = "Subtitle";
 var GE8wrk = "Image";
 var Gzx6q5 = "Advertising";
+var K5oXVa = "AnswerNo";
+var MhGUaY = "Slideshow";
 var N25iDO = "Title";
 var OdcEna = "No slide...";
 var PKCv5M = "No item...";
+var PQG8A5 = "Link";
+var PbHeHc = "Markers";
+var RrCcE4 = "Gallery feed";
+var RvCDph = "Failure";
 var S9YpbV = "Text";
+var Spu1AH = "MapPath";
+var StDSlO = "Ad";
+var SzDJB6 = "Retry";
 var Tdnwlv = "Items";
 var Tt4jN1 = "Add";
 var UBXIYP = "Video";
+var UGHvSn = "Options";
+var YG5ohI = "Success";
+var Zpl5bJ = "Spacing";
 var ZxgNge = "Add an item";
-var aA8bDw = "Text";
 var aovRFH = "Filename";
+var b7jY9K = "Map";
+var bhEaUt = "Audio";
 var d60tSn = "Subtitle";
 var dVcog7 = "Title";
+var dmuKH3 = "Map";
+var eIiKqX = "Slides";
+var ebsPd4 = "Image";
 var enFs4g = "Advanced";
 var eoW0tZ = "Duration";
+var h11vXq = "Question?";
+var jaFXki = "Gallery";
+var l6RRDj = "AnswerYes";
 var liyV7Y = "Size";
 var mv37oK = "Dimension";
 var n2teag = "Add an answer";
+var nqQqsI = "Map";
 var p6n6vY = "No answer...";
 var qKQCxc = "Add a marker";
 var qWT7a5 = "TimelineCentered";
+var r6ZYoE = "SurveyMultipleChoice";
 var tAYwqp = "Add a slide";
 var tvl2Zc = "Video";
+var uJ9eBc = "Image";
+var vrCfAS = "GalleryFeed";
+var yUPjCz = "Slideshow";
+var z4CiV1 = "Text";
 var messages = {
-  "+0zv6g": "Image",
   "+MPZRu": "Background",
+  "169xnt": "Audio",
   "2/F1Q7": "#{index}",
   "2ZOPe+": "Title",
   "3E57H7": "No marker...",
+  "3pqFXb": "Button style",
+  "3q9GPv": "Quote",
   "4E2gbX": "Text",
   "4iBXj2": "Layout",
   "507VAi": "Description",
+  "5uH4x7": "Submit",
+  "63Kvho": "Images",
+  "6kvjpX": "SurveyYesNo",
+  "72Z3Od": "Multiple answers",
+  "73hxYw": "Author",
+  "88/Ge8": "Quote",
+  "9wk4/T": "Source",
   "B1MeY+": "Select an image...",
   DWEzCv: DWEzCv,
   EcZYpr: EcZYpr,
   GE8wrk: GE8wrk,
   Gzx6q5: Gzx6q5,
+  K5oXVa: K5oXVa,
+  "KGHyr/": "SurveyCheckbox",
+  MhGUaY: MhGUaY,
   N25iDO: N25iDO,
   OdcEna: OdcEna,
   PKCv5M: PKCv5M,
+  PQG8A5: PQG8A5,
+  PbHeHc: PbHeHc,
+  RrCcE4: RrCcE4,
+  RvCDph: RvCDph,
   S9YpbV: S9YpbV,
+  Spu1AH: Spu1AH,
+  StDSlO: StDSlO,
+  SzDJB6: SzDJB6,
   Tdnwlv: Tdnwlv,
   Tt4jN1: Tt4jN1,
   UBXIYP: UBXIYP,
+  UGHvSn: UGHvSn,
   "V+G2s+": "Add an image",
+  YG5ohI: YG5ohI,
+  Zpl5bJ: Zpl5bJ,
   ZxgNge: ZxgNge,
-  aA8bDw: aA8bDw,
   aovRFH: aovRFH,
+  b7jY9K: b7jY9K,
+  bhEaUt: bhEaUt,
   "c9VwT/": "Technical details",
   d60tSn: d60tSn,
   dVcog7: dVcog7,
+  dmuKH3: dmuKH3,
   "e6+aGf": "Select a media...",
+  eIiKqX: eIiKqX,
+  ebsPd4: ebsPd4,
   enFs4g: enFs4g,
   eoW0tZ: eoW0tZ,
+  h11vXq: h11vXq,
+  jaFXki: jaFXki,
+  l6RRDj: l6RRDj,
   liyV7Y: liyV7Y,
   mv37oK: mv37oK,
   n2teag: n2teag,
+  nqQqsI: nqQqsI,
   p6n6vY: p6n6vY,
   qKQCxc: qKQCxc,
   qWT7a5: qWT7a5,
-  "schema.properties.answer_no": "Answer No",
-  "schema.properties.answer_yes": "Answer Yes",
-  "schema.properties.answers": "Answers",
-  "schema.properties.audio": "Audio",
-  "schema.properties.author": "Author",
-  "schema.properties.background": "Background",
-  "schema.properties.button": "Button style",
-  "schema.properties.button-style": "Button style",
-  "schema.properties.cardBackground": "Item background",
-  "schema.properties.cardTextStyle": "Marker text style",
-  "schema.properties.failure": "Failure",
-  "schema.properties.image": "Image",
-  "schema.properties.images": "Images",
-  "schema.properties.layout": "Layout",
-  "schema.properties.map": "Map",
-  "schema.properties.markers": "Markers",
-  "schema.properties.question": "Question",
-  "schema.properties.quote": "Quote",
-  "schema.properties.slides": "Slides",
-  "schema.properties.source": "Source",
-  "schema.properties.success": "Success",
-  "schema.properties.text": "Text",
-  "schema.properties.text_style": "Text style",
-  "schema.title": "Survey with yes or no",
-  "screen.no": "No",
-  "screen.retry": "Retry",
-  "screen.submit": "Submit",
-  "screen.yes": "Yes",
+  r6ZYoE: r6ZYoE,
   "t+L1J/": "No image...",
   tAYwqp: tAYwqp,
   tvl2Zc: tvl2Zc,
-  "wt1+dM": "Select an audio file..."
+  uJ9eBc: uJ9eBc,
+  "uOFw+A": "Map with path",
+  vrCfAS: vrCfAS,
+  "wt1+dM": "Select an audio file...",
+  yUPjCz: yUPjCz,
+  z4CiV1: z4CiV1
 };
 
 /* eslint-disable */
