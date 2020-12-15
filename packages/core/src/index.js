5a9bb55c21ea8d4cb84af4b4d61c64c81fa7489e
David Mongeau-Petitpas <dmongeau.petitpas@gmail.com>
2020-12-15 12:33:13 -0500

    Fix with export

--- packages/core/src/index.js
+++ packages/core/src/index.js
@@ -1,6 +1,10 @@
+export * from '@micromag/core/hooks';
+export * from '@micromag/core/contexts';
+export * from '@micromag/core/components';
+export * from '@micromag/core/utils';
 export * as PropTypes from './PropTypes';
-export * from './hooks';
-export * from './contexts';
-export * from './components';
-export * from './utils';
+//
+// export * from './contexts';
+// export * from './components';
+// export * from './utils';
 export * from './lib';
