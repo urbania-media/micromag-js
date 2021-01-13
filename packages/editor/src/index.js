49659507c19c6a7dae475a2c1f0f6f476b3e6c76
Nicolas Roy B <nrb@atelierfolklore.ca>
2021-01-13 11:59:11 -0500

    Editor export

--- packages/editor/src/index.js
+++ packages/editor/src/index.js
@@ -2,3 +2,4 @@ export { default } from './components/EditorContainer';
 export { default as Editor } from './components/Editor';
 export { default as Screens } from './components/menus/Screens';
 export { default as ScreenTypes } from './components/menus/ScreenTypes';
+export { default as createScreen } from './utils/createScreen';
