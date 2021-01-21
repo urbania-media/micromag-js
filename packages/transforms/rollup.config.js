4d9c90f2ef65c0f88cd0148cbadac107777c758a
Nicolas Roy B <nrb@atelierfolklore.ca>
2021-01-21 11:21:57 -0500

    test

--- packages/transforms/rollup.config.js
+++ packages/transforms/rollup.config.js
@@ -62,38 +62,3 @@ export default Object.keys(files).reduce(
     ],
     [],
 );
-
-// import path from 'path';
-// import resolve from '@rollup/plugin-node-resolve';
-
-// import { createConfig } from '../../rollup.config';
-
-// export default [
-//     createConfig({
-//         resolveOptions: {
-//             extensions: ['.mjs', '.js', '.jsx', '.json', '.node'],
-//             resolveOnly: [new RegExp(path.join(__dirname, './src'))],
-//         },
-//     }),
-//     createConfig({
-//         format: 'cjs',
-//         resolveOptions: {
-//             extensions: ['.mjs', '.js', '.jsx', '.json', '.node'],
-//             resolveOnly: [new RegExp(path.join(__dirname, './src'))],
-//         },
-//     }),
-//     createConfig({
-//         file: 'apple-news.js',
-//     }),
-//     createConfig({
-//         file: 'apple-news.js',
-//         format: 'cjs',
-//     }),
-//     createConfig({
-//         file: 'utils.js',
-//     }),
-//     createConfig({
-//         file: 'utils.js',
-//         format: 'cjs',
-//     }),
-// ];
