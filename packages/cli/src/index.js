b0f9f66bc1759954a6c91b7f0b38be588e685086
David Mongeau-Petitpas <dmongeau.petitpas@gmail.com>
2021-01-22 11:03:36 -0500

    Added index

---
+++ packages/cli/src/index.js
@@ -0,0 +1,3 @@
+export { default as transformStory } from './utils/transformStory';
+export { default as captureStory } from './utils/captureStory';
+export { default as getStoryHtml } from './utils/getStoryHtml';
