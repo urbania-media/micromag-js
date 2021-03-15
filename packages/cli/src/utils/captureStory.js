ec5485fdbdfac451348983fddb38f352b2cf6b71
Nicolas Roy B <nrb@atelierfolklore.ca>
2021-03-15 17:13:52 -0400

    test

--- packages/cli/src/utils/captureStory.js
+++ packages/cli/src/utils/captureStory.js
@@ -34,49 +34,49 @@ const captureStory = async (story, location, settings = {}) => {
         args: ['--no-sandbox'],
     });
 
-    try {
-        const pages = await browser.pages();
-        const hasPage = pages.length > 0;
-        const page = hasPage ? pages[0] : await browser.newPage();
-        await page.goto(`http://127.0.0.1:${serverPort}`);
+    // try {
+    const pages = await browser.pages();
+    const hasPage = pages.length > 0;
+    const page = hasPage ? pages[0] : await browser.newPage();
+    await page.goto(`http://127.0.0.1:${serverPort}`);
 
-        const { components: screens = null } = story || {};
-        if (screens !== null) {
-            const count = screens.length;
-            for (let index = 0; index < count; index += 1) {
-                const { id, type } = screens[index];
-                const singleScreenStory = { ...story, components: screens.slice(index, index + 1) };
-                console.log(`Screen ${index + 1}/${count} (${type})...`);
-                await page.evaluate(
-                    (storyToRender, storyProps) => renderStory(storyToRender, storyProps),
-                    singleScreenStory,
-                    {
-                        screen: id,
-                        renderContext: 'capture',
-                        withoutRouter: true,
-                        withoutMenu: true,
-                        width,
-                        height,
-                        googleApiKey,
-                    },
-                );
+    const { components: screens = null } = story || {};
+    if (screens !== null) {
+        const count = screens.length;
+        for (let index = 0; index < count; index += 1) {
+            const { id, type } = screens[index];
+            const singleScreenStory = { ...story, components: screens.slice(index, index + 1) };
+            console.log(`Screen ${index + 1}/${count} (${type})...`);
+            await page.evaluate(
+                (storyToRender, storyProps) => renderStory(storyToRender, storyProps),
+                singleScreenStory,
+                {
+                    screen: id,
+                    renderContext: 'capture',
+                    withoutRouter: true,
+                    withoutMenu: true,
+                    width,
+                    height,
+                    googleApiKey,
+                },
+            );
 
-                try {
-                    await page.waitForSelector('[data-screen-ready="true"]', {
-                        timeout: READY_WAIT_TIMEOUT,
-                    });
-                } catch (e) {
-                    console.log(`Timeout reached: ${id}`);
-                }
-
-                await page.screenshot({
-                    path: getOutputPath(location, `${index}_${id}.png`),
+            try {
+                await page.waitForSelector('[data-screen-ready="true"]', {
+                    timeout: READY_WAIT_TIMEOUT,
                 });
+            } catch (e) {
+                console.log(`Timeout reached: ${id}`);
             }
+
+            await page.screenshot({
+                path: getOutputPath(location, `${index}_${id}.png`),
+            });
         }
-    } catch (e) {
-        console.log('ERROR', e); // eslint-disable-line
     }
+    // } catch (e) {
+    //     console.log('ERROR', e); // eslint-disable-line
+    // }
 
     await browser.close();
     server.close();
