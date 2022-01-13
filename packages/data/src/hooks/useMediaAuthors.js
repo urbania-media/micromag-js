5d3d2b379e4824a5e7392c13bb241934bbc7b4d1
Nicolas Roy B <nrb@atelierfolklore.ca>
2022-01-13 17:48:36 -0500

    Debug

--- packages/data/src/hooks/useMediaAuthors.js
+++ packages/data/src/hooks/useMediaAuthors.js
@@ -1,17 +1,19 @@
-import { useCallback } from 'react';
+// import { useCallback } from 'react';
 import { useApi } from '../contexts/ApiContext';
-import useItems from './useItems';
+// import useItems from './useItems';
 
+// eslint-disable-next-line
 const useMediaAuthors = (query = null, count = 5, opts) => {
     const api = useApi();
-    const getItems = useCallback(() => api.medias.getAuthors(query, count), [api, query, count]);
-    const { items, pageItems, ...request } = useItems({
-        getItems,
-        ...opts,
-    });
+    console.log('api', api); // eslint-disable-line
+    // const getItems = useCallback(() => api.medias.getAuthors(query, count), [api, query, count]);
+    // const { items, pageItems, ...request } = useItems({
+    //     getItems,
+    //     ...opts,
+    // });
     return {
-        authors: items,
-        ...request,
+        authors: [],
+        // ...request,
     };
 };
 
--- packages/data/src/index.js
+++ packages/data/src/index.js
@@ -1,5 +1,5 @@
-export * from './contexts/ApiContext';
-export { default as DataProvider } from './DataProvider';
 export * from './hooks/index';
+export * from './contexts/ApiContext';
 export { default as Api } from './lib/Api';
 export { default as BaseApi } from './lib/Base';
+export { default as DataProvider } from './DataProvider';
