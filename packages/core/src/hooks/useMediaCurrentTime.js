ba5d75688787177851e078b163206175a6828c2c
David Mongeau-Petitpas <dmongeau.petitpas@gmail.com>
2022-06-10 13:37:58 -0400

    Fix

--- packages/core/src/hooks/useMediaCurrentTime.js
+++ packages/core/src/hooks/useMediaCurrentTime.js
@@ -2,53 +2,51 @@ import { useState, useEffect, useRef } from 'react';
 
 function useMediaCurrentTime(
     element,
-    {
-        id = null,
-        disabled = false,
-        updateInterval = 1000,
-        onUpdate: customOnUpdate = null,
-    } = {},
+    { id = null, disabled = false, updateInterval = 1000, onUpdate: customOnUpdate = null } = {},
 ) {
     const [currentTime, setCurrentTime] = useState(0);
     const realCurrentTime = useRef(currentTime);
 
-    const lastIdRef = useRef(id);
-    const idChanged = lastIdRef.current !== id;
+    const finalId = id || element;
+    const lastIdRef = useRef(finalId);
+    const idChanged = lastIdRef.current !== finalId;
     if (idChanged) {
         realCurrentTime.current = element !== null ? element.currentTime || 0 : 0;
-        lastIdRef.current = id;
+        lastIdRef.current = finalId;
     }
 
     // Check time update
     useEffect(() => {
-        if (disabled || element === null) {
+        if (element === null) {
             return () => {};
         }
-        let canceled = false;
-        function updateTime(time) {
-            if (canceled) {
-                return;
-            }
-            realCurrentTime.current = time;
-            setCurrentTime(time);
-
-            if (customOnUpdate !== null) {
-                customOnUpdate(time);
-            }
-        };
-        function onInterval() {
+        function updateTime() {
             const time = element.currentTime;
-            if (typeof time.then !== 'undefined') {
-                time.then(updateTime);
-            } else {
-                updateTime(time);
+            if (time !== realCurrentTime.current) {
+                realCurrentTime.current = time;
+                setCurrentTime(time);
+
+                if (customOnUpdate !== null) {
+                    customOnUpdate(time);
+                }
             }
-        };
-        const interval = setInterval(onInterval, updateInterval);
-        onInterval();
+            return time;
+        }
+        if (disabled) {
+            updateTime();
+            return () => {};
+        }
+        let timeout = null;
+        function loop() {
+            const time = updateTime();
+            const remainingTime = Math.floor((element.duration - time) * 1000);
+            timeout = setTimeout(loop, Math.min(updateInterval, remainingTime));
+        }
+        loop();
         return () => {
-            canceled = true;
-            clearInterval(interval);
+            if (timeout !== null) {
+                clearInterval(timeout);
+            }
         };
     }, [id, element, setCurrentTime, disabled, updateInterval]);
 
--- packages/core/src/hooks/useMediaDuration.js
+++ packages/core/src/hooks/useMediaDuration.js
@@ -5,11 +5,12 @@ function useMediaDuration(element, { id = null } = {}) {
 
     const realDuration = useRef(duration);
 
-    const lastIdRef = useRef(id);
-    const idChanged = lastIdRef.current !== id;
+    const finalId = id || element;
+    const lastIdRef = useRef(finalId);
+    const idChanged = lastIdRef.current !== finalId;
     if (idChanged) {
         realDuration.current = element !== null ? element.duration || 0 : 0;
-        lastIdRef.current = id;
+        lastIdRef.current = finalId;
     }
 
     useEffect(() => {
@@ -17,13 +18,18 @@ function useMediaDuration(element, { id = null } = {}) {
             return () => {};
         }
         function updateDuration() {
-            realDuration.current = element.duration || 0;
-            setDuration(element.duration || 0);
+            const newDuration = element.duration || 0;
+            if (newDuration !== realDuration.current) {
+                realDuration.current = newDuration;
+                setDuration(newDuration);
+            }
         }
         updateDuration();
+        element.addEventListener('canplay', updateDuration);
         element.addEventListener('loadedmetadata', updateDuration);
         element.addEventListener('durationchange', updateDuration);
         return () => {
+            element.removeEventListener('canplay', updateDuration);
             element.removeEventListener('loadedmetadata', updateDuration);
             element.removeEventListener('durationchange', updateDuration);
         };
