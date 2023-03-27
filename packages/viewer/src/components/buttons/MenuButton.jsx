18c95c81f0ff6bca384acf2af44cdb323c086351
Joseph Blais <joseph@atelierfolklore.ca>
2023-03-27 17:04:01 -0400

    Update icons

--- packages/viewer/src/components/buttons/MenuButton.jsx
+++ packages/viewer/src/components/buttons/MenuButton.jsx
@@ -37,16 +37,16 @@ const MenuButton = ({ className, iconClassName, ...props }) => {
                     width="19"
                     height="23"
                     viewBox="0 0 19 23"
-                    // fill="none"
+                    fill="none"
                     xmlns="http://www.w3.org/2000/svg"
                 >
-                    <mask id="path-1-inside-1_526_17237" fill="currentColor">
+                    <mask id="path-1-inside-1_572_15622" fill="currentColor">
                         <path d="M0 7C0 4.79086 1.79086 3 4 3H12C14.2091 3 16 4.79086 16 7V19C16 21.2091 14.2091 23 12 23H4C1.79086 23 0 21.2091 0 19V7Z" />
                     </mask>
                     <path
                         d="M0 3H16H0ZM16 19C16 21.7614 13.7614 24 11 24H4C1.23858 24 -1 21.7614 -1 19H1C1 20.6569 2.34315 22 4 22H12C14.2091 22 16 20.6569 16 19ZM4 24C1.23858 24 -1 21.7614 -1 19V8C-1 5.23858 1.23858 3 4 3C2.34315 3 1 4.79086 1 7V19C1 20.6569 2.34315 22 4 22V24ZM16 3V23V3Z"
                         fill="currentColor"
-                        mask="url(#path-1-inside-1_526_17237)"
+                        mask="url(#path-1-inside-1_572_15622)"
                     />
                     <rect x="3.5" y="0.5" width="15" height="19" rx="2.5" stroke="currentColor" />
                 </svg>
--- packages/viewer/src/components/partials/ArrowHint.jsx
+++ packages/viewer/src/components/partials/ArrowHint.jsx
@@ -16,17 +16,19 @@ const ArrowHint = ({ className }) => (
     <div className={classNames([styles.container, { [className]: className !== null }])}>
         <div className={styles.inner}>
             <svg
-                className={styles.arrow}
+                width="20"
+                height="14"
+                viewBox="0 0 20 14"
+                fill="none"
                 xmlns="http://www.w3.org/2000/svg"
-                xmlnsXlink="http://www.w3.org/1999/xlink"
-                x="0px"
-                y="0px"
-                width="10px"
-                height="16px"
-                viewBox="0 0 10 16"
-                fill="currentColor"
+                className={styles.arrow}
             >
-                <polygon points="9.62 4.62 5 0 0.38 4.62 1.44 5.68 4.25 2.87 4.25 14.39 5.75 14.39 5.75 2.87 8.56 5.68 9.62 4.62" />
+                <path
+                    fillRule="evenodd"
+                    clipRule="evenodd"
+                    d="M13.353 0.371014L19.319 6.33704C19.6851 6.70314 19.6851 7.29671 19.319 7.66282L13.353 13.6288C12.9869 13.9949 12.3933 13.9949 12.0272 13.6288C11.6611 13.2627 11.6611 12.6692 12.0272 12.3031L16.3929 7.9374H0V6.06246H16.3929L12.0272 1.6968C11.6611 1.33069 11.6611 0.737119 12.0272 0.371014C12.3933 0.0049094 12.9869 0.0049094 13.353 0.371014Z"
+                    fill="currentColor"
+                />
             </svg>
         </div>
     </div>
