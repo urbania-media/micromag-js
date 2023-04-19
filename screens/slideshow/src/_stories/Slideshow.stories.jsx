13cad152744f5300462f6df38120bbf9e90c9356
Joseph Blais <joseph@atelierfolklore.ca>
2023-04-19 10:41:42 -0400

    Update slideshow

--- screens/slideshow/src/_stories/Slideshow.stories.jsx
+++ screens/slideshow/src/_stories/Slideshow.stories.jsx
@@ -12,24 +12,25 @@ import SlideshowScreen from '../Slideshow';
 import definition from '../definition';
 
 const props = {
+    // Note: the api call in the imageMedia() function will return the same image every time
     // slides: [...Array(3).keys()].map(() => ({ media: imageMedia(), caption: text() })),
     slides: [
         {
-            body: 'heyooo',
+            caption: { body: 'heyooo' },
             media: {
                 type: 'image',
                 url: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.x1cF5E2z-cDvdwobCd7E7AHaHa%26pid%3DApi&f=1&ipt=26aa4f08bef63304cced4483b1b73041be30c8c825265c1a09f84aa44c677861&ipo=images',
             },
         },
         {
-            body: 'heyooo2',
+            caption: { body: 'heyooo2' },
             media: {
                 type: 'image',
                 url: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn.pixabay.com%2Fphoto%2F2012%2F05%2F04%2F10%2F18%2Ftwo-47085_1280.png&f=1&nofb=1&ipt=3e467f5b8eb3cd6a532bdd8054112704b125d67a0305fbe1a2b0b6fe360ee616&ipo=images',
             },
         },
         {
-            body: 'heyooo3',
+            caption: { body: 'heyooo3' },
             media: {
                 type: 'image',
                 url: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi1.wp.com%2Fgeekgabpodcasts.com%2Fgiggabpodcast%2Fwp-content%2Fuploads%2Fsites%2F2%2F2017%2F08%2Fglitzy-number-3.jpg&f=1&nofb=1&ipt=435f0a31fb576c39a66e5b2313685eacee6ae7a6a7c7dc58d90dca98f232c931&ipo=images',
