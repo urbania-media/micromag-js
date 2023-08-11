f60ffed7ead17214efa7d4ee702ec834852e4ac8
Nicolas Roy B <phatshambler@gmail.com>
2023-08-11 13:38:40 -0400

    Working copy

--- scripts/lib/getPackagesPaths.js
+++ scripts/lib/getPackagesPaths.js
@@ -1,12 +1,12 @@
 const path = require('path');
 const globSync = require('glob').sync;
-const lerna = require('../../lerna.json');
+const packageJson = require('../../package.json');
 
 const rootDir = path.join(__dirname, '../../');
 
 const getPackagesPaths = () =>
-    lerna.packages
-        .map(it => it.replace(/\/\*/, '/'))
+    packageJson.workspaces
+        .map((it) => it.replace(/\/\*/, '/'))
         .reduce(
             (paths, packagesPath) => [
                 ...paths,
