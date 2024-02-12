fb7ee4327308827768454ca0fa2d958f8a65e847
Joseph Blais <joseph@atelierfolklore.ca>
2024-02-12 09:45:51 -0500

    update

--- .storybook/data/stories/urbania-article-card.js
+++ .storybook/data/stories/urbania-article-card.js
@@ -1,8 +1,6 @@
 import { v4 as uuid } from 'uuid';
 
-import { backgroundVideo } from '../../data';
-
-console.log('dfg', backgroundVideo());
+// import { backgroundVideo } from '../../data';
 
 export default {
     id: uuid(),
