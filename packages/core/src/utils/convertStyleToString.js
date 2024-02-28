96a0e319514f15e3081ae4471b9d08d343749e47
David Mongeau-Petitpas <dmongeau.petitpas@gmail.com>
2024-02-28 10:08:22 -0500

    Fix

--- packages/core/src/utils/convertStyleToString.js
+++ packages/core/src/utils/convertStyleToString.js
@@ -1,12 +1,12 @@
 import isNumber from 'lodash/isNumber';
-import { paramCase } from 'change-case';
+import { kebabCase } from 'change-case';
 
 const convertStyleToString = style =>
     style !== null
         ? Object.keys(style)
               .map(
                   key =>
-                      `${paramCase(key)}:${isNumber(style[key]) ? `${style[key]}px` : style[key]};`,
+                      `${kebabCase(key)}:${isNumber(style[key]) ? `${style[key]}px` : style[key]};`,
               )
               .join('\n')
         : '';
