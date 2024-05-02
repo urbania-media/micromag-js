94bc46071ee799c6d78900a7df567d0b20be21b1
Nicolas Roy B. <phatshambler@gmail.com>
2024-05-02 09:49:58 -0400

    Media field

--- packages/fields/src/components/Media.jsx
+++ packages/fields/src/components/Media.jsx
@@ -1,90 +1,107 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import PropTypes from 'prop-types';
 import React, { useCallback } from 'react';
-import { FormattedMessage } from 'react-intl';
 
-import { PropTypes as MicromagPropTypes } from '@micromag/core';
-import { getFileName } from '@micromag/core/utils';
-import MediaGallery from '@micromag/media-gallery';
+// import { FormattedMessage } from 'react-intl';
+// import { PropTypes as MicromagPropTypes } from '@micromag/core';
+// import { getFileName } from '@micromag/core/utils';
+import MediaModal from './MediaModal';
 
-import FieldWithForm from './FieldWithForm';
+// import MediaGallery from '@micromag/media-gallery';
+
+// import FieldWithForm from './FieldWithForm';
 
 const propTypes = {
-    type: PropTypes.oneOfType([
-        MicromagPropTypes.mediaTypes,
-        PropTypes.arrayOf(MicromagPropTypes.mediaTypes),
-    ]),
-    value: MicromagPropTypes.media,
-    noValueLabel: MicromagPropTypes.label,
-    withoutThumbnail: PropTypes.bool,
+    // type: PropTypes.oneOfType([
+    //     MicromagPropTypes.mediaTypes,
+    //     PropTypes.arrayOf(MicromagPropTypes.mediaTypes),
+    // ]),
+    // value: MicromagPropTypes.media,
+    // noValueLabel: MicromagPropTypes.label,
+    // withoutThumbnail: PropTypes.bool,
     onChange: PropTypes.func,
     closeForm: PropTypes.func,
 };
 
 const defaultProps = {
-    type: null,
-    value: null,
-    noValueLabel: (
-        <FormattedMessage
-            defaultMessage="Select a media..."
-            description="Label when no value is provided to Media field"
-        />
-    ),
-    withoutThumbnail: false,
+    // type: null,
+    // value: null,
+    // noValueLabel: (
+    //     <FormattedMessage
+    //         defaultMessage="Select a media..."
+    //         description="Label when no value is provided to Media field"
+    //     />
+    // ),
+    // withoutThumbnail: false,
     onChange: null,
     closeForm: null,
 };
 
 const MediaField = ({
-    type,
-    value,
-    noValueLabel,
-    withoutThumbnail,
-    onChange,
+    // type,
+    // value,
+    // noValueLabel,
+    // withoutThumbnail,
+    // onChange,
     closeForm,
+    onChange,
     ...props
 }) => {
-    const onClickMedia = useCallback(
-        (media) => {
+    // const onClickMedia = useCallback(
+    //     (media) => {
+    //         if (onChange !== null) {
+    //             onChange(media !== null && value !== null && media.id === value.id ? null : media);
+    //         }
+    //         if (closeForm !== null) {
+    //             // NOTE: enable this for auto-back on select media
+    //             // closeForm();
+    //         }
+    //     },
+    //     [value, onChange, closeForm],
+    // );
+
+    // const onClearMedia = useCallback(() => {
+    //     if (onChange !== null) {
+    //         onChange(null);
+    //     }
+    // }, [value, onChange, closeForm]);
+
+    // const label = value !== null ? value.name || getFileName(value.url) || null : null;
+
+    const closeOnChange = useCallback(
+        (newValue) => {
             if (onChange !== null) {
-                onChange(media !== null && value !== null && media.id === value.id ? null : media);
+                onChange(newValue);
             }
             if (closeForm !== null) {
-                // NOTE: enable this for auto-back on select media
-                // closeForm();
+                console.log('close');
+                closeForm();
             }
         },
-        [value, onChange, closeForm],
+        [onChange, closeForm],
     );
 
-    const onClearMedia = useCallback(() => {
-        if (onChange !== null) {
-            onChange(null);
-        }
-    }, [value, onChange, closeForm]);
-
-    const label = value !== null ? value.name || getFileName(value.url) || null : null;
-
     return (
-        <FieldWithForm
-            value={value}
-            onChange={onChange}
-            noValueLabel={noValueLabel}
-            label={label}
-            withTitleLabel={label !== null}
-            thumbnailPath="thumbnail_url"
-            {...props}
-        >
-            <MediaGallery
-                type={type}
-                isPicker
-                isSmall
-                selectedMedia={value}
-                onChange={onChange}
-                onClickMedia={onClickMedia}
-                onClearMedia={onClearMedia}
-            />
-        </FieldWithForm>
+        <MediaModal onChange={closeOnChange} {...props} />
+        // <FieldWithForm
+        //     value={value}
+        //     onChange={onChange}
+        //     noValueLabel={noValueLabel}
+        //     label={label}
+        //     withTitleLabel={label !== null}
+        //     thumbnailPath="thumbnail_url"
+        //     {...props}
+        // >
+        //     <MediaGallery
+        //         type={type}
+        //         isPicker
+        //         isSmall
+        //         selectedMedia={value}
+        //         onChange={onChange}
+        //         onClickMedia={onClickMedia}
+        //         onClearMedia={onClearMedia}
+        //     />
+        // </FieldWithForm>
     );
 };
 
