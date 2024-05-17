c0a7da862ef2dd900d468120d75769087598019c
Stéphane Lepin <stephane.lepin@gmail.com>
2024-05-17 19:44:53 +0200

    MediaGallery: re-enable withReplace

--- packages/media-gallery/src/components/MediaGallery.jsx
+++ packages/media-gallery/src/components/MediaGallery.jsx
@@ -205,7 +205,7 @@ function MediaGallery({
                     multipleSelection // allow multi-uploads in MediasBrowser
                     withStickySelection
                     withTrash
-                    // withReplace // TODO re-enable after prod deploy
+                    withReplace
                 />
             )}
         </div>
