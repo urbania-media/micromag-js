84200636efa922a3d4c514cd363aa2a6b2554925
Nicolas Roy B. <phatshambler@gmail.com>
2025-12-16 13:05:51 -0500

    Triggers

--- elements/scroll/src/Scroll.jsx
+++ elements/scroll/src/Scroll.jsx
@@ -38,7 +38,7 @@ const defaultProps = {
     scrolleeClassName: null,
     children: null,
     scrollPosition: null,
-    triggers: [0.25, 0.5, 0.75, 1.0],
+    triggers: [0.1, 0.25, 0.5, 0.75, 0.9, 1.0],
     onScrolledTrigger: null,
     onScrolledBottom: null,
     onScrolledNotBottom: null,
