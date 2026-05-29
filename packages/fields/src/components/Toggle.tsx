372be58e6a5d9893c341ee9e6bf20f405cd4871d
David Mongeau-Petitpas <dmongeau.petitpas@gmail.com>
2026-05-29 13:46:57 -0400

    Fix

--- packages/fields/src/components/Toggle.tsx
+++ packages/fields/src/components/Toggle.tsx
@@ -1,34 +1,56 @@
 import classNames from 'classnames';
-import Switch from 'rc-switch';
+import { HTMLAttributes } from 'react';
 
-import styles from '../styles/toggle.module.css';
-import 'rc-switch/assets/index.css';
-
-interface ToggleFieldProps {
+interface ToggleFieldProps extends Omit<
+    HTMLAttributes<HTMLInputElement>,
+    'value' | 'defaultValue' | 'onChange'
+> {
+    inputId?: string | null;
     value?: boolean | null;
     defaultValue?: boolean | string | null;
     className?: string | null;
     disabled?: boolean;
-    onChange?: ((...args: unknown[]) => void) | null;
+    onChange?: ((newValue: boolean) => void) | null;
 }
 
 function ToggleField({
+    inputId = null,
     value = null,
     defaultValue = null,
     className = null,
     onChange = null,
+    disabled = false,
     ...props
 }: ToggleFieldProps) {
     const finalValue =
         value === null && (defaultValue === true || defaultValue === 'true') ? true : value;
     return (
-        <Switch
-            {...props}
-            className={classNames([styles.container, className])}
-            checked={finalValue !== null ? finalValue : false}
-            onChange={onChange}
-        />
+        <div className={classNames(['form-check form-switch fs-4 p-0', className])}>
+            <input
+                {...props}
+                id={inputId}
+                switch
+                className="form-check-input m-0"
+                type="checkbox"
+                role="switch"
+                checked={finalValue}
+                disabled={disabled}
+                onChange={(e) => {
+                    if (onChange !== null) {
+                        onChange(e.target.checked);
+                    }
+                }}
+            />
+        </div>
     );
+    // return (
+    //     <Switch
+    //         {...props}
+    //         className={classNames([styles.container, className])}
+    //         checked={finalValue !== null ? finalValue : false}
+    //         onChange={onChange}
+    //     />
+    // );
 }
 
 ToggleField.isHorizontal = true;
